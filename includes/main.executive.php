a6d9bcec111668886569b373ce462e50e15a8f06
TsukasakiTomohide <89874104+TsukasakiTomohide@users.noreply.github.com>
2021-09-21 04:09:03 -0500

    Update main.executive.php

--- includes/main.executive.php
+++ includes/main.executive.php
@@ -9,7 +9,7 @@
 
     $_SESSION['usersposition'] = 'executive';
     echo($_SESSION['usersposition']);
-    echo("4");
+    echo("3");
     echo($_SESSION['usersemail']);
     echo("5");
     // Nobody can directly enter this page except executive. Login is necessary.
