adb60c793f9c165029697b93959f835e5664f2db
TsukasakiTomohide <89874104+TsukasakiTomohide@users.noreply.github.com>
2021-09-22 18:48:36 -0500

    Update testSendGrid2.php

--- testSendGrid2.php
+++ testSendGrid2.php
@@ -2,7 +2,7 @@
    require_once 'config.php';
 // require 'vendor/autoload.php'; // If you're using Composer (recommended)
 // Comment out the above line if not using Composer
-// require("<PATH TO>/sendgrid-php.php");
+ require("./sendgrid/sendgrid-php.php");
 // If not using Composer, uncomment the above line and
 // download sendgrid-php.zip from the latest release here,
 // replacing <PATH TO> with the path to the sendgrid-php.php file,
