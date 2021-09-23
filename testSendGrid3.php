9d6be75b3efc8d55437897e080e28081f524f369
TsukasakiTomohide <89874104+TsukasakiTomohide@users.noreply.github.com>
2021-09-23 10:48:06 -0500

    Create testSendGrid3.php

---
+++ testSendGrid3.php
@@ -0,0 +1,13 @@
+<?php
+
+$userid = 'ttsukasaki@hiokiusa.com';
+$password = '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'
+
+$sendgrid = new SendGrid($userid, $password);
+$mail = new SendGridMail();
+$mail->addTo('tsukasaki.tomohide.com@gmail.com')->
+       setFrom('ttsukasaki@hiokiusa.com')->
+       setSubject('Subject goes here')->
+       setText('Hello World!')->
+       setHtml('<strong>Hello World!</strong>');
+$sendgrid->smtp->send($mail);
