96161e5be25335e419c7345bfc99adf0033b6403
TsukasakiTomohide <89874104+TsukasakiTomohide@users.noreply.github.com>
2021-09-23 13:26:25 -0500

    Update testSendGrid4.php

--- testSendGrid4.php
+++ testSendGrid4.php
@@ -1,13 +1,49 @@
 <?php
 
-$username  = 'apikey';
-$password = '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';
+$url = 'https://api.sendgrid.com/';
+$user = 'ttsukasaki@hiokiusa.com';
+$pass = ''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'';
 
-$sendgrid = new SendGrid($username, $password);
-$mail = new SendGridMail();
-$mail->addTo('tsukasaki.tomohide.com@gmail.com')->
-       setFrom('ttsukasaki@hiokiusa.com')->
-       setSubject('Subject goes here')->
-       setText('Hello World!')->
-       setHtml('<strong>Hello World!</strong>');
-$sendgrid->smtp->send($mail);
+$json_string = array(
+
+  'to' => array(
+    'ttsukasaki@hiokiusa.com', 'tsukasaki.tomohide.com@gmail.com'
+  ),
+  'category' => 'test_category'
+);
+
+
+$params = array(
+    'api_user'  => $user,
+    'api_key'   => $pass,
+    'x-smtpapi' => json_encode($json_string),
+    'to'        => 'example3@sendgrid.com',
+    'subject'   => 'testing from curl',
+    'html'      => 'testing body',
+    'text'      => 'testing body',
+    'from'      => 'ttsukasaki@hiokiusa.com',
+  );
+
+
+$request =  $url.'api/mail.send.json';
+
+// Generate curl request
+$session = curl_init($request);
+// Tell curl to use HTTP POST
+curl_setopt ($session, CURLOPT_POST, true);
+// Tell curl that this is the body of the POST
+curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+// Tell curl not to return headers, but do return the response
+curl_setopt($session, CURLOPT_HEADER, false);
+// Tell PHP not to use SSLv3 (instead opting for TLS)
+curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
+curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+// obtain response
+$response = curl_exec($session);
+curl_close($session);
+
+// print everything out
+print_r($response);
+
+?>
