8204ae620579e512b3dd364cc7c8a9bb88ace841
TsukasakiTomohide <89874104+TsukasakiTomohide@users.noreply.github.com>
2021-09-23 10:57:26 -0500

    Update testSendGrid3.php

--- testSendGrid3.php
+++ testSendGrid3.php
@@ -1,7 +1,7 @@
 <?php
 
 $userid   = 'ttsukasaki@hiokiusa.com';
-$password = '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';
+$password = 'MasakoShota08051019';
 
 $sendgrid = new SendGrid($userid, $password);
 $mail = new SendGridMail();
