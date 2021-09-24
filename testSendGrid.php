f9e1b4bdc02223d992b3b20cc0c5cfc691a49ed5
TsukasakiTomohide <89874104+TsukasakiTomohide@users.noreply.github.com>
2021-09-24 13:50:30 -0500

    Update testSendGrid.php

--- testSendGrid.php
+++ testSendGrid.php
@@ -1,28 +1,11 @@
 <?php
-require_once 'config.php';
-// require 'vendor/autoload.php'; // If you're using Composer (recommended)
-// Comment out the above line if not using Composer
-// require("<PATH TO>/sendgrid-php.php");
-// If not using Composer, uncomment the above line and
-// download sendgrid-php.zip from the latest release here,
-// replacing <PATH TO> with the path to the sendgrid-php.php file,
-// which is included in the download:
-// https://github.com/sendgrid/sendgrid-php/releases
 
-$email = new \SendGrid\Mail\Mail(); 
-$email->setFrom("ttsukasaki@hiokiusa.com", "Example User");
-$email->setSubject("This is a test email.");
-$email->addTo("tsukasaki.tomohide.com@gmail.com", "Example User");
-$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-$email->addContent(
-    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-);
-$sendgrid = new \SendGrid(SENDGRID_API_KEY);
-try {
-    $response = $sendgrid->send($email);
-    print $response->statusCode() . "\n";
-    print_r($response->headers());
-    print $response->body() . "\n";
-} catch (Exception $e) {
-    echo 'Caught exception: '. $e->getMessage() ."\n";
-}
+require_once 'sendMail.php';
+
+$senderEmail    = 'ttsukasaki@hiokiusa.com';
+$senderName     = 'Tsukasaki Sender';
+$receiverEmail  = 'tsukasaki.tomohide.com@gmail.com';
+$receiverName   = 'Tsukasaki Receiver';
+$subject        = 'Subject: Test Email';
+$body           = 'Body: This is a test email';
+sendMail($senderEmail, $senderName, $receiverEmail, $receiverName, $subject, $body);
