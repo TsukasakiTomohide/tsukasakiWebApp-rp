70b6e5bcb8ed4dd0a37a7746681d3c48eb6629ad
TsukasakiTomohide <89874104+TsukasakiTomohide@users.noreply.github.com>
2021-09-24 13:50:43 -0500

    Update testSendGrid2.php

--- testSendGrid2.php
+++ testSendGrid2.php
@@ -20,9 +20,9 @@ $email->addContent(
 $sendgrid = new \SendGrid(SENDGRID_API_KEY);
 try {
     $response = $sendgrid->send($email);
-    print $response->statusCode() . "\n";
-    print_r($response->headers());
-    print $response->body() . "\n";
+    //print $response->statusCode() . "\n";
+    //print_r($response->headers());
+    //print $response->body() . "\n";
 } catch (Exception $e) {
     echo 'Caught exception: '. $e->getMessage() ."\n";
 }
