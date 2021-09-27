4dcac7309916afd5f68d49440f3270b8679d9ee6
TsukasakiTomohide <89874104+TsukasakiTomohide@users.noreply.github.com>
2021-09-27 14:10:44 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,5 @@
 <?php
     define('SENDGRID_API_KEY', 
     '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');
-    define('SENDGRID_USERNAME', 'ttsukasaki@hiokiusa.com');
+    define('WildCardPW', 
+    '123456Aa%');
