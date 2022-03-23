0b661ce8abe789e731dd6748529433f1f47de3c7
TsukasakiTomohide <89874104+TsukasakiTomohide@users.noreply.github.com>
2022-03-23 15:09:55 -0500

    Create backup.php

---
+++ includes/backup.php
@@ -0,0 +1,18 @@
+<?php
+    // Nobody can directly go to Administrator.php. Login is necessary.
+    if($_SESSION['usersposition'] != 'administrator' && $_SESSION['usersposition'] != 'executive'){
+        header("location: ../login.php?error=adminerror");
+        exit();
+     }
+
+     $myfile = fopen("test.txt", "r") or die("Unable to open file!");
+     while(!feof($myfile)) {
+        echo fgets($myfile) . "<br>";
+      }
+     fclose($myfile);
+
+     $myfile = fopen("test.txt", "w");
+     fclose($myfile);
+
+?>
+
