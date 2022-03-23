6a6a287b8f0a27302456cefbb2d6808e5c2073fe
TsukasakiTomohide <89874104+TsukasakiTomohide@users.noreply.github.com>
2022-03-23 15:48:20 -0500

    Update admin.backup.php

--- includes/admin.backup.php
+++ includes/admin.backup.php
@@ -15,34 +15,51 @@
     getBackup($conn, $year);
 
     // *************** Download *************** //
-     FileDownload();
+    download("test.txt");
      
     // ************* Back to Admin ************ //
     header("location: ./backup.php");
 
 
     // *************** Sub-Function of Download *************** //
-    function FileDownload(){
-        // File name to be downloaded
-        $filepath = 'test.txt';
-          
-        // File name after $filepath is downloaded
-        $filename = 'test.txt';
-          
-        // File type
-        // header('Content-Type: application/force-download');
-          
-        // Get the file size to show the progress of download
-        // header('Content-Length: '.filesize($filepath));
-          
-        // Request download and rename
-        // header('Content-Disposition: attachment; filename="'.$filename.'"');
-          
-        // Read the file and start download
-        // readfile($filepath);
+    function download($pPath, $pMimeType = null)
+    {
+        //-- ファイルが読めない時はエラー(もっときちんと書いた方が良いが今回は割愛)
+        if (!is_readable($pPath)) { die($pPath); }
     
-        exit;
+        //-- Content-Typeとして送信するMIMEタイプ(第2引数を渡さない場合は自動判定) ※詳細は後述
+        // $mimeType = (isset($pMimeType)) ? $pMimeType
+        //                                : (new finfo(FILEINFO_MIME_TYPE))->file($pPath);
+    
+        //-- 適切なMIMEタイプが得られない時は、未知のファイルを示すapplication/octet-streamとする
+        if (!preg_match('/\A\S+?\/\S+/', $mimeType)) {
+            $mimeType = 'application/octet-stream';
         }
+    
+        //-- Content-Type
+        header('Content-Type: ' . $mimeType);
+    
+        //-- ウェブブラウザが独自にMIMEタイプを判断する処理を抑止する
+        header('X-Content-Type-Options: nosniff');
+    
+        //-- ダウンロードファイルのサイズ
+        header('Content-Length: ' . filesize($pPath));
+    
+        //-- ダウンロード時のファイル名
+        header('Content-Disposition: attachment; filename="' . basename($pPath) . '"');
+    
+        //-- keep-aliveを無効にする
+        header('Connection: close');
+    
+        //-- readfile()の前に出力バッファリングを無効化する ※詳細は後述
+        while (ob_get_level()) { ob_end_clean(); }
+    
+        //-- 出力
+        readfile($pPath);
+    
+        //-- 最後に終了させるのを忘れない
+        exit;
+    }
 ?>
 
 
