58cc28f417a5cee2a360162e4e890bec69ffda6e
TsukasakiTomohide <ttsukasaki@hiokiusa.com>
2022-11-28 15:34:20 -0600

    Update main.manager.php

--- includes/main.manager.php
+++ includes/main.manager.php
@@ -2,11 +2,9 @@
     include_once 'header.php';
     require_once 'dbh.inc.php';
     require_once 'vc.functions.php';
-
-    //if(!isset($_SESSION)){
-    //    session_start();
-    //}
-
+    if(!isset($_SESSION)){
+        session_start();
+     }
     // Nobody can directly enter this page except manager. Login is necessary.
     if($_SESSION['usersposition'] != 'manager'){
         header("location: ../login.php?error=adminerror");
