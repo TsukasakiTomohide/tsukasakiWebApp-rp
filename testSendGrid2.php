2a3934a231aba76a200632d23947ac649ca72ad2
TsukasakiTomohide <ttsukasaki@hiokiusa.com>
2022-12-05 21:10:27 -0600

    Update testSendGrid2.php

--- testSendGrid2.php
+++ testSendGrid2.php
@@ -30,12 +30,12 @@ try {
 
 $apiKey = "API KEY";
 $client = new SendGridClient(apiKey);
-$from = new EmailAddress("登録した送信者のメールアドレス@example.com", "fromの名前");
+$from = new EmailAddress("ttsukasaki@hiokiusa.com", "fromの名前");
 $subject = "SendGridを使ったメール送信";
-$to = new EmailAddress("to@example.com", "toの名前");
+$to = new EmailAddress("tsukasaki.tomohide.com@gmail.com", "toの名前");
 $plainTextContent = "テキストの内容です。";
 $htmlContent = "<strong>HTMLの内容です。</strong>";
-$msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+$msg = MailHelper.CreateSingleEmail($from, $to, $subject, $plainTextContent, $htmlContent);
 
 $response = client.SendEmailAsync(msg).ConfigureAwait(false);
 //Console.WriteLine(response.StatusCode.ToString());
