3004cdb5ddd2715eeb414f9b3f771a51a8312621
TsukasakiTomohide <ttsukasaki@hiokiusa.com>
2022-12-05 22:29:16 -0600

    Update testSendGrid2.php

--- testSendGrid2.php
+++ testSendGrid2.php
@@ -1,16 +1,2 @@
 <?php
-
-print("a");
-/*
-$apiKey = "API KEY";
-$client = new SendGridClient($apiKey);
-$from = new EmailAddress("ttsukasaki@hiokiusa.com", "fromの名前");
-$subject = "SendGridを使ったメール送信";
-$to = new EmailAddress("tsukasaki.tomohide.com@gmail.com", "toの名前");
-$plainTextContent = "テキストの内容です。";
-$htmlContent = "<strong>HTMLの内容です。</strong>";
-$msg = MailHelper.CreateSingleEmail($from, $to, $subject, $plainTextContent, $htmlContent);
-
-$response = $client.SendEmailAsync(msg).ConfigureAwait(false);
-print($response.StatusCode.ToString());
-*/
+echo("a");
